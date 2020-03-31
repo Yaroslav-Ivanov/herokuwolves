50647e55e6b839881dba2f84b60e9f6dc99ca985
Yaroslav-Ivanov <yaroslavvitebsk@gmail.com>
2020-03-31 21:03:05 +0300

    Update first.php

--- first.php
+++ first.php
@@ -7,7 +7,22 @@
 </head>
 <body>
     <?php
-    echo "Hello World"
+/* Username: UN4HX42TJA
+
+Database name: UN4HX42TJA
+
+Password: bVdy8vW8wY
+
+Server: remotemysql.com
+
+Port: 3306*/
+
+$link = mysqli_connect("remotemysql.com", " UN4HX42TJA", "bVdy8vW8wY")
+    or die("Не удалось соединится с сервером");
+
+
+
+    // echo "Hello World"
 
     
     ?>
